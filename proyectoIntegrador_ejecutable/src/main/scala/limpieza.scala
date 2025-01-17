2fd4d127b92e58a886fee5af9907ac391b86aced
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2025-01-17 00:32:06 -0500

    AVANCE LIMPIEZA

--- proyectoIntegrador_ejecutable/src/main/scala/limpieza.scala
+++ proyectoIntegrador_ejecutable/src/main/scala/limpieza.scala
@@ -30,32 +30,186 @@ object limpieza extends App {
 
   // FUNCION PARA LIMPIEZA DE JSON
   def cleanJson(json: String): String = {
-    json
-      .trim
-      .replaceAll("'", "\"") // CAMBIA COMILLAS SIMPLES POR DOBLES
-      .replaceAll("None", "null") // CAMBIA NONE POR NULL
-      .replaceAll("True", "true") // NORMALIZAR BOOLEANO
-      .replaceAll("False", "false") //NORMALIZAR BOOLEANO
-      .replaceAll("""\\""", "") // ELIMINA ESPACIOS
+      json
+        .trim
+        .replaceAll("'", "\"") // CAMBIA COMILLAS SIMPLES POR DOBLES
+        .replaceAll("None", "null") // CAMBIA NONE POR NULL
+        .replaceAll("True", "true") // NORMALIZAR BOOLEANO
+        .replaceAll("False", "false") //NORMALIZAR BOOLEANO
+        .replaceAll("""\\""", "") // ELIMINA ESPACIOS
   }
 
   data_limpia = filas_validas.map { movie =>
     //LIMPIAR ADULT
-    val adult_limpio = movie.adult.toString.toLowerCase match {
+    val adult_limpio = movie.adult.toString.trim.toLowerCase match {
       case "true" | "t" | "1" => true
       case "false" | "f" | "0" | "" => false
       case _ => false
     }
 
-    //LIMPIAR COLUMNA JSON
-    //LIMPIAR BELONGS_TO_COLLECTION
-    val belongs_to_collection = Json.parse(Option(movie.belongs_to_collection)
-      .filter(_.trim.nonEmpty).map(cleanJson).getOrElse("{}")).toString()
+    //LIMPIAR VIDEO
+    val video_limpio = movie.video.toString.trim.toLowerCase match {
+      case "true" | "t" | "1" => true
+      case "false" | "f" | "0" | "" => false
+      case _ => false
+    }
 
+    // LIMPIAR BELONGS_TO_COLLECTION
+    val belongsToCollection_limpio = if (movie.belongs_to_collection == null || movie.belongs_to_collection.trim.isEmpty) {
+      "{}"
+    } else {
+      val cadena_original = movie.belongs_to_collection.trim
+      if ((!cadena_original.endsWith("}"))&&(!cadena_original.startsWith("{"))) {
+          "{" + cleanJson(cadena_original) + "}"
+      } else if (!cadena_original.endsWith("}")) {
+          cleanJson(cadena_original) + "}"
+      } else if (!cadena_original.startsWith("{")) {
+          "{" + cleanJson(cadena_original)
+      } else {
+          cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR GENRES
+    val genres_limpio = if (movie.genres == null || movie.genres.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.genres.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR PRODUCTION COMPANIES
+    val production_companies_limpio = if (movie.production_companies == null || movie.production_companies.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.production_companies.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR PRODUCTION COUNTRIES
+    val production_countries_limpio = if (movie.production_countries == null || movie.production_countries.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.production_countries.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR SPOKEN_LANGUAGES
+    val spoken_languages_limpio = if (movie.spoken_languages == null || movie.spoken_languages.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.spoken_languages.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR KEYWORDS
+    val keywords_limpio = if (movie.keywords == null || movie.keywords.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.keywords.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR CAST
+    val cast_limpio = if (movie.cast == null || movie.cast.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.cast.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR CREW
+    val crew_limpio = if (movie.crew == null || movie.crew.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.crew.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
+
+    // LIMPIAR RATINGS
+    val ratings_limpio = if (movie.ratings == null || movie.ratings.trim.isEmpty) {
+      "[]"
+    } else {
+      val cadena_original = movie.ratings.trim
+      if ((!cadena_original.endsWith("]"))&&(!cadena_original.startsWith("["))) {
+        "[" + cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.endsWith("]")) {
+        cleanJson(cadena_original) + "]"
+      } else if (!cadena_original.startsWith("[")) {
+        "[" + cleanJson(cadena_original)
+      } else {
+        cleanJson(cadena_original)
+      }
+    }
 
     movie.copy(
       adult = adult_limpio,
-      belongs_to_collection = belongs_to_collection,
+      video = video_limpio,
+      belongs_to_collection = belongsToCollection_limpio,
+      genres = genres_limpio,
+      production_companies = production_companies_limpio,
+      production_countries = production_countries_limpio,
+      spoken_languages = spoken_languages_limpio,
+      keywords = keywords_limpio,
+      cast = cast_limpio,
+      crew = crew_limpio,
+      ratings = ratings_limpio
     )
   }
 
